12df628717eed38560fe094f4ac53fd6d2e80939
joanartdacosta <joanartdacosta@gmail.com>
2024-03-21 11:44:42 +0100

    create footer

---
+++ app/src/components/common/footer/socials.js
@@ -0,0 +1,16 @@
+const SOCIALS = [
+  {
+    id: 1,
+    href: "https://www.instagram.com/chalet_designinteriores/",
+    text: "instagram-icon",
+    imgURL: "https://chalet-webapp.s3.amazonaws.com/icons/insta-icon.png",
+  },
+  {
+    id: 2,
+    href: "/contacts",
+    text: "message-icon",
+    imgURL: "https://chalet-webapp.s3.amazonaws.com/icons/message-icon.png",
+  },
+];
+
+export default SOCIALS;
