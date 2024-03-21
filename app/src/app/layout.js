756b03ceed121e0c103240b3b8879bb12a537359
joanartdacosta <joanartdacosta@gmail.com>
2024-03-21 14:00:51 +0100

    add component to homepage - quote

--- app/src/app/layout.js
+++ app/src/app/layout.js
@@ -16,7 +16,6 @@ export default function RootLayout({ children }) {
       <body>
         <NavBar />
         {children}
-        <Footer />
       </body>
     </html>
   );
---
+++ app/src/app/page.js
@@ -0,0 +1,13 @@
+import Background from "@/components/dashboard/home/Background";
+import Quote from "@/components/dashboard/home/Quote";
+
+export default function Home() {
+  return (
+    <main>
+      <div>
+        <Background />
+        <Quote />
+      </div>
+    </main>
+  );
+}
