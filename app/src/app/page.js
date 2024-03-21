d982bde7612154e60c7e642e05f9fb7b53aacc8c
joanartdacosta <joanartdacosta@gmail.com>
2024-03-21 11:55:45 +0100

    add components to home

---
+++ app/src/app/page.js
@@ -0,0 +1,11 @@
+import Background from "@/components/dashboard/home/Background";
+
+export default function Home() {
+  return (
+    <main>
+      <div>
+        <Background />
+      </div>
+    </main>
+  );
+}
