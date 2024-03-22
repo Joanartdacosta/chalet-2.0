dd9ee55c05de3eca4352275d20af278c2e0fb6bf
joanartdacosta <joanartdacosta@gmail.com>
2024-03-22 12:12:56 +0100

    including text's and advantages list

---
+++ app/src/app/about/page.js
@@ -0,0 +1,11 @@
+import AboutDesigner from "@/components/dashboard/about/AboutDesigner";
+import Advantages from "@/components/dashboard/advantages/Advantage";
+
+export default function About() {
+  return (
+    <div className="margin-auto max-width padding-top-5">
+      <AboutDesigner />
+      <Advantages />
+    </div>
+  );
+}
---
+++ app/src/components/dashboard/about/designer.js
@@ -0,0 +1,22 @@
+const DESIGNER = [
+  {
+    id: 1,
+    title: "designer photo",
+    imgURL:
+      "https://chalet-webapp.s3.amazonaws.com/designer-photos/IMG_0970+(2).JPG",
+  },
+  {
+    id: 2,
+    title: "designer photo2",
+    imgURL:
+      "https://chalet-webapp.s3.amazonaws.com/designer-photos/IMG_4135.JPG",
+  },
+  {
+    id: 3,
+    title: "designer photo3",
+    imgURL:
+      "https://chalet-webapp.s3.amazonaws.com/designer-photos/IMG_1148.JPG",
+  },
+];
+
+export default DESIGNER;
