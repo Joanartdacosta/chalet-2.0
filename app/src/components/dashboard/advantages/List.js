595c4055faf341d7418dee8e3e83b072736be378
joanartdacosta <joanartdacosta@gmail.com>
2024-03-22 12:09:57 +0100

    add a contact me section

--- app/src/components/dashboard/advantages/List.js
+++ app/src/components/dashboard/advantages/List.js
@@ -1,5 +1,4 @@
-import HandHeart from "../../../../public/HandHeart";
-import AdvantagesList from "./AdvantageList";
+import ContactMe from "./ContactMe";
 
 import CONTACT_ME from "@/components/dashboard/advantages/contact_me";
 
@@ -7,7 +6,7 @@ export default function List() {
   return (
     <div className="pb-20 pt-20 h-screen text-center ">
       <p className="pb-10">ENTRE EM CONTACTO</p>
-      <AdvantagesList items={CONTACT_ME} />
+      <ContactMe items={CONTACT_ME} />
     </div>
   );
 }
