ad4ab8abb63e392185cf040dd665775f7189db43
joanartdacosta <joanartdacosta@gmail.com>
2024-06-07 10:59:11 +0200

    import component carousel

--- app/src/app/page.js
+++ app/src/app/page.js
@@ -1,7 +1,8 @@
 import Background from "@/components/dashboard/home/Background";
+import Carousel from "@/components/dashboard/home/Carousel";
 import List from "@/components/dashboard/advantages/List";
 import Quote from "@/components/dashboard/home/Quote";
-
+import REVIEWS from "@/components/lists/reviews";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
         <Background />
         <Quote />
         <List />
+        <Carousel list={REVIEWS} />
       </div>
     </main>
   );
