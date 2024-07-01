9e64498d40008fc558603bc763d5807051a06f09
joanartdacosta <joanartdacosta@gmail.com>
2024-07-01 21:36:24 +0200

    question - about me section

---
+++ app/src/components/dashboard/about/Question.js
@@ -0,0 +1,21 @@
+export default function Question() {
+  return (
+    <div className="m-auto p-60">
+      <h1 className="text-5xl pb-4">Mas quais as</h1>
+      <h1 className="text-5xl pb-4 ml-10">
+        <span className="text-custom-darkBrown font-bold pl-10">
+          Vantagens{" "}
+        </span>
+        de contratar
+      </h1>
+
+      <h1 className="pl-40 text-5xl pb-4">
+        um{" "}
+        <span className="italic text-7xl text-custom-darkBrown">
+          Designer de Interiores
+        </span>
+        <span className="italic text-7xl text-black"> ?</span>
+      </h1>
+    </div>
+  );
+}
