07064df4c9e473e68e5daff080366496b1a668f4
joanartdacosta <joanartdacosta@gmail.com>
2024-07-01 20:05:39 +0200

    cleaning about and use componentes in about designer - about me section

--- app/src/app/about/page.js
+++ app/src/app/about/page.js
@@ -1,11 +1,9 @@
 import AboutDesigner from "@/components/dashboard/about/AboutDesigner";
-import Advantages from "@/components/dashboard/advantages/Advantage";
 
 export default function About() {
   return (
-    <div className="margin-auto max-width padding-top-5">
+    <div className="m-auto max-w-5xl justify-center">
       <AboutDesigner />
-      <Advantages />
     </div>
   );
 }
