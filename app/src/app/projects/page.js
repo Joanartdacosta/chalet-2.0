f30260ba9d6e454c837f317b286c83df3c9e5a2d
joanartdacosta <joanartdacosta@gmail.com>
2024-07-05 19:32:23 +0200

    project card component - projects section

---
+++ app/src/app/projects/page.js
@@ -0,0 +1,12 @@
+import CardProject from "@/components/dashboard/projects/Card";
+
+export default function Projects() {
+  return (
+    <div className="m-auto max-w-5xl justify-center">
+      <p className="p-10 text-2xl opensans text-center">PROJETOS</p>
+      <div className="">
+        <CardProject />
+      </div>
+    </div>
+  );
+}
