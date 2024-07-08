90d55ce72f2e0916cfa45103c3bc1384f75dc3e2
joanartdacosta <joanartdacosta@gmail.com>
2024-07-08 23:43:13 +0200

    contacts page

---
+++ app/src/app/contacts/page.js
@@ -0,0 +1,10 @@
+import Form from "@/components/form/Form";
+
+export default function Contacts() {
+  return (
+    <div className="m-auto max-w-5xl justify-center">
+      <p className="p-10 text-2xl opensans text-center">CONTACTO</p>
+      <Form />
+    </div>
+  );
+}
