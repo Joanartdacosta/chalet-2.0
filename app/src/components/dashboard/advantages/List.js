2c3864eb089dd7d12386abb5a8ff985e13dc2f4f
joanartdacosta <joanartdacosta@gmail.com>
2024-07-09 12:34:53 +0200

    fix error

--- app/src/components/dashboard/advantages/List.js
+++ app/src/components/dashboard/advantages/List.js
@@ -1,11 +1,9 @@
 import AdvantagesList from "./AdvantageList";
-import ADVANTAGES_LIST from "./advantage_list";
-
 export default function List() {
   return (
     <div className="pb-20 pt-20 h-screen text-center ">
       <p className="pb-10 text-2xl opensans">ENTRE EM CONTACTO</p>
-      <AdvantagesList items={CONTACT_ME} />
+      <AdvantagesList />
     </div>
   );
 }
