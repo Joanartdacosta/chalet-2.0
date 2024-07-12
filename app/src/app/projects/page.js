506eef619d23f68ae2a93aa443208f105a50cb4c
joanartdacosta <joanartdacosta@gmail.com>
2024-07-12 13:46:06 +0200

    add projectName to card component

--- app/src/app/projects/page.js
+++ app/src/app/projects/page.js
@@ -4,7 +4,7 @@ export default function Projects() {
   return (
     <div className="m-auto max-w-5xl justify-center">
       <p className="p-10 text-2xl opensans text-center">PROJETOS</p>
-      <CardProject />
+      <CardProject projectName={"PROJECTO OEIRAS"} />
     </div>
   );
 }
