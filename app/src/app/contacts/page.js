7582a05c76750f7d6f9d62c863ada46ae7dccb62
joanartdacosta <joanartdacosta@gmail.com>
2024-07-15 11:04:09 +0200

    delete 'contactos' title

--- app/src/app/contacts/page.js
+++ app/src/app/contacts/page.js
@@ -3,7 +3,6 @@ import Form from "@/components/form/Form";
 export default function Contacts() {
   return (
     <div className="m-auto max-w-5xl justify-center">
-      <p className="p-10 text-2xl opensans text-center">CONTACTO</p>
       <Form />
     </div>
   );
