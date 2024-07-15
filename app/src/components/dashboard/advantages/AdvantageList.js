0e79c33810a23deaf6a66fb21bb8a11d9bcfd6ae
joanartdacosta <joanartdacosta@gmail.com>
2024-07-15 14:06:35 +0200

    add flipcard to advantages section and complete about me page with all components

--- app/src/components/dashboard/advantages/AdvantageList.js
+++ app/src/components/dashboard/advantages/AdvantageList.js
@@ -1,11 +1,12 @@
-import CardAdvantage from "./card/CardAdvantage";
+import FlipCard from "./card/FlipCard";
 
 export default function AdvantagesList(props) {
   return (
-    <div className="flex flex-wrap gap-2 max-w-4xl m-auto">
+    <div className="flex flex-wrap gap-2 max-w-4xl m-auto justify-center">
       {props.items.map((item) => {
         return (
-          <CardAdvantage
+          <FlipCard
+            key={item.id}
             imgURL={item.imgURL}
             description={item.description}
             title={item.title}
