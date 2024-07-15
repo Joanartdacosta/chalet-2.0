f305faa9f6814453dee276c1188a2d65b24bb666
joanartdacosta <joanartdacosta@gmail.com>
2024-07-15 10:59:41 +0200

    include priority image

--- app/src/components/form/Form.js
+++ app/src/components/form/Form.js
@@ -24,6 +24,7 @@ function Form() {
             src="https://images.unsplash.com/photo-1523865236457-3ae3358a4eaa?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             width={600}
             height={100}
+            priority={true}
           />
           <div className="p-4">
             <p className="pb-2">
