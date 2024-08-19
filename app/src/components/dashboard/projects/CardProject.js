1949781b0069b382535976b794e8aa3d148c2400
joanartdacosta <joanartdacosta@gmail.com>
2024-08-19 12:33:19 +0200

    card project for project list

--- app/src/components/dashboard/projects/CardProject.js
+++ app/src/components/dashboard/projects/CardProject.js
@@ -1,4 +1,5 @@
 import Button from "@/components/common/buttons/Button";
+import generateRandomKey from "@/components/utils/generate_random_key";
 import Image from "next/legacy/image";
 
 export default function CardProject(props) {
@@ -9,15 +10,15 @@ export default function CardProject(props) {
           className="absolute mix-blend-overlay object-cover"
           layout="fill"
           priority={false}
-          src="https://chalet-webapp.s3.amazonaws.com/homepage/MezzanineHall-background.png"
-          alt="chalet_mezanine"
+          src={props.imgURL}
+          alt={props.projectName + "_" + generateRandomKey()}
         />
         <div className="pt-20 pb-20 relative text-center">
           <h3 className="pb-5 ropasans text-5xl text-white tracking-wider">
             {props.projectName}
           </h3>
           <div className="m-auto lg:w-72 w-[8rem]">
-            <Button href={"/projects/1"} label={"VER GALERIA"} />
+            <Button href={`/projects/${props.slug}`} label={"VER GALERIA"} />
           </div>
         </div>
       </div>
