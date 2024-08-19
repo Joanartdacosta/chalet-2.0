19db2d9789075c3505213c73c62d60c11ed516dd
joanartdacosta <joanartdacosta@gmail.com>
2024-08-19 13:15:37 +0200

    load data from projects.json

--- app/src/app/projects/[projectId]/page.js
+++ app/src/app/projects/[projectId]/page.js
@@ -1,9 +1,31 @@
 import ProjectsGallery from "@/components/dashboard/projects/ProjectsGallery";
+import projects from "@/app/data/projects.json";
+import path from "path";
+import { promises as fs } from "fs";
 
-export default function ProjectDetailPage() {
+export default async function ProjectDetailPage({ params }) {
+  const { projectId } = params;
+  const filePath = path.join(process.cwd(), "src/app/data/projects.json");
+
+  const fileContents = await fs.readFile(filePath, "utf-8");
+
+  const projects = JSON.parse(fileContents);
+
+  const project = projects.find((p) => p.id.toString() === projectId) || null;
+
+  if (!project) {
+    return <div>Project not found</div>;
+  }
+
   return (
     <div className="pt-24">
-      <ProjectsGallery />
+      <ProjectsGallery
+        id={project.id}
+        href={project.href}
+        projectName={project.projectName}
+        projectType={project.projectType}
+        gallery={project.gallery}
+      />
     </div>
   );
 }
