5f558a51a3db9af9aac1b9b0d28d403d6e061308
joanartdacosta <joanartdacosta@gmail.com>
2024-08-26 22:46:34 +0200

    reviews

--- app/src/lists/reviews.js
+++ app/src/lists/reviews.js
@@ -1,31 +1,26 @@
 const REVIEWS = [
   {
     name: "Sadeline S.",
-    citation: "2023, Sintra",
     quote:
       "Quando se trabalha com o coração e se junta o talento e o bom gosto, os resultados só podem superar as expectativas! Obrigada Mariana pelo excelente trabalho, pelo cuidado, por cada detalhe pensado ao pormenor! Ficou ainda mais bonito do que imaginámos!",
   },
   {
     name: "Joana P. e Francisco C.",
-    citation: "2024, Lisboa",
     quote:
       "Quando decidimos pedir ajuda à Mariana, era apenas para uma parte da casa e acabámos por pedir mais. Nunca achei que fizesse tanta diferença ter uma ajuda de alguém especializado, sempre alinhada com os nossos gostos e considerando as dinâmicas de dia-a-dia dos espaços, comodidade e acessibilidade. Recomendo a 100% os seus serviços, fiquei muito impressionada com a rapidez e alinhamento.",
   },
   {
     name: "Maria G.",
-    citation: "2023, Lisboa",
     quote:
       "Quando contratámos a Mariana, já tínhamos comprado o mobiliário principal para a suite, contudo sentimos que precisávamos de ajuda para efetivamente decorar a divisão! Adorámos o projeto que a Mariana nos apresentou, totalmente dentro do estilo e cores que desejámos! Achamos também muito importante ela dar várias opções de escolha dentro do mesmo artigo! Iremos de certeza recomendar os seus serviços a amigos e familiares!.",
   },
   {
     name: "Gonçalo P.",
-    citation: "2024, Lisboa",
     quote:
       "Gostámos muito! Havia várias coisas que já tínhamos idealizado e acho que a Mariana conseguiu captar isso mesmo bem nas reuniões iniciais dos dois projetos. Depois passar essas ideias para um projeto visível e o trabalho todo de pesquisa foi fantástico!",
   },
   {
     name: "Cátia C",
-    citation: "2024, Lisboa",
     quote:
       "O que achei bastante interessante no trabalho da Mariana é que ela captou muito rapidamente o nosso estilo e deu-nos flexibilidade para escolher dentro do estilo. Soluções simples, que fazem a diferença em orçamentos reduzidos. Na nossa situação, que temos uma casa arrendada, consigo perfeitamente ver que todas as peças que a Mariana propôs podem encaixar em outra casa no futuro. E isto tudo sem deixarmos de nos sentir em casa! É mesmo giro começar a ver os móveis e as peças a chegar (comprámos quase tudo online), encaixar as pecinhas conforme o plano, com as dimensões que ela especificou e ver que tudo encaixa e fica ainda melhor que no digital!",
   },
