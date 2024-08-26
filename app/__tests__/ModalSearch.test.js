b7d2ee1aa2a50d526d3a34a7f3caccf8348d573a
joanartdacosta <joanartdacosta@gmail.com>
2024-08-26 23:29:13 +0200

    delete unecessary info

--- app/__tests__/ModalSearch.test.js
+++ app/__tests__/ModalSearch.test.js
@@ -1,35 +1,23 @@
 import { render, fireEvent, screen } from "@testing-library/react";
 import ModalSearch from "@/components/dashboard/services/ModalSearch";
 
-const mockServices = [
-  { projectType: "Design", type: "Design" },
-  { projectType: "Decor", type: "Decor" },
-  { projectType: "Home Staging", type: "Staging" },
-];
-
 const setServices = jest.fn();
 
-describe("ModalSearch Component", () => {
-  beforeEach(() => {
-    jest.clearAllMocks();
-  });
+test("renders input and button correctly", () => {
+  render(<ModalSearch setServices={setServices} />);
 
-  test("renders input and button correctly", () => {
-    render(<ModalSearch setServices={setServices} />);
+  expect(
+    screen.getByPlaceholderText("Procure pelo servico que pretende")
+  ).toBeInTheDocument();
+  expect(screen.getByRole("button")).toBeInTheDocument();
+});
 
-    expect(
-      screen.getByPlaceholderText("Procure pelo servico que pretende")
-    ).toBeInTheDocument();
-    expect(screen.getByRole("button")).toBeInTheDocument();
-  });
+test("updates input value correctly", () => {
+  render(<ModalSearch setServices={setServices} />);
 
-  test("updates input value correctly", () => {
-    render(<ModalSearch setServices={setServices} />);
-
-    const input = screen.getByPlaceholderText(
-      "Procure pelo servico que pretende"
-    );
-    fireEvent.change(input, { target: { value: "Design" } });
-    expect(input.value).toBe("Design");
-  });
+  const input = screen.getByPlaceholderText(
+    "Procure pelo servico que pretende"
+  );
+  fireEvent.change(input, { target: { value: "Design" } });
+  expect(input.value).toBe("Design");
 });
