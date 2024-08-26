a54bc8c64dfc0715f720d314d9c7ebb3612e2e8a
joanartdacosta <joanartdacosta@gmail.com>
2024-08-26 22:30:05 +0200

    font size navbar links

--- app/src/components/common/link/NavLink.js
+++ app/src/components/common/link/NavLink.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function NavLink(props) {
   return (
     <>
-      <Link className="text-xl text-black no-underline pr-10" href={props.href}>
+      <Link className="text-lg text-black no-underline pr-10" href={props.href}>
         {props.label}
       </Link>
     </>
