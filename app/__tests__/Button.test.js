2283100bbd7d2b07b9cfca565f16e333c1a5afd0
joanartdacosta <joanartdacosta@gmail.com>
2024-08-26 23:07:01 +0200

    dynamic href

--- app/__tests__/Button.test.js
+++ app/__tests__/Button.test.js
@@ -3,9 +3,10 @@ import Button from "@/components/common/buttons/Button";
 
 describe("Button Component", () => {
   test("renders a button with href prop and navigates to the correct URL", () => {
-    render(<Button href="/test-url" label="Test Link" />);
+    const dynamicHref = "/projects";
+    render(<Button href={dynamicHref} label="/Projects" />);
     const buttonElement = document.querySelector("a");
-    expect(buttonElement).toHaveAttribute("href", "/test-url");
+    expect(buttonElement).toHaveAttribute("href", "/projects");
   });
 
   test("renders a button with onClick prop and handles click event", () => {
